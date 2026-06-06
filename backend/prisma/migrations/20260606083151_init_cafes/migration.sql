8332b5f7a969267a3698ce7c78557b0e8f74f164
Sean Boonzaier <boonzaiersa@gmail.com>
2026-06-06 11:42:53 +0100

    Generate initial prisma config and first schema

---
+++ backend/prisma/migrations/20260606083151_init_cafes/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Cafe" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Cafe_pkey" PRIMARY KEY ("id")
+);
