a5462bc80a09f5524ca12793f3a93ad49bb7788f
Sean Boonzaier <boonzaiersa@gmail.com>
2026-06-08 14:30:44 +0100

    Update fetchAllCafes to use postgres database

--- backend/src/services/cafe.service.ts
+++ backend/src/services/cafe.service.ts
@@ -1,3 +1,5 @@
+import type { QueryResult } from "pg";
+import { query } from "../config/db.js";
 const cafes = ["Insomnia", "Cafe Nero", "Wall & Keogh", "Brewlabs"];
 
 // Should fetch within a certain radius of the given one
@@ -12,8 +14,24 @@ export const fetchCafesByLocation = async (
 // Allow for an optional limit to be given
 // Needs to ensure you understand pagination
 export const fetchAllCafes = async (limit?: number) => {
-  console.log(`Limit: ${limit}`);
-  return limit ? cafes.slice(0, limit) : cafes;
+  try {
+    const sql = `
+      SELECT
+        cafe_name as name,
+        cafe_description as description,
+        ST_X(cafe_location) as latitude,
+        ST_Y(cafe_location) as longitude
+      FROM cafes
+      `;
+
+    const result = await query(sql).then((result: QueryResult) => {
+      return result.rows;
+    });
+    return result;
+  } catch (error) {
+    console.log("Database query error:", error);
+    throw error;
+  }
 };
 
 export const fetchCafesWithBeansByBrand = async (beanBrand: string) => {
