598e00cb72da74f83fdd615a45b7cbd7fc8b8e06
Sean Boonzaier <boonzaiersa@gmail.com>
2026-06-08 14:21:11 +0100

    Create db pool config with query helper

---
+++ backend/src/config/db.ts
@@ -0,0 +1,21 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const query = async (text: string, params?: any[]) => {
+  const start = Date.now();
+  try {
+    const res = await pool.query(text, params);
+    const duration = Date.now() - start;
+    console.log(`Execute Query: ${text} [${duration}ms]`);
+    return res;
+  } catch (error) {
+    console.log(`Database query error:`, error);
+    throw error;
+  }
+};
