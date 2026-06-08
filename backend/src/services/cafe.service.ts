44885b5fb1d2a5e1ab73a5f6c9bb459bcb5c8579
Sean Boonzaier <boonzaiersa@gmail.com>
2026-06-08 11:55:50 +0100

    Add cafe fetching placeholder functions

--- backend/src/services/cafe.service.ts
+++ backend/src/services/cafe.service.ts
@@ -1,5 +1,7 @@
 const cafes = ["Insomnia", "Cafe Nero", "Wall & Keogh", "Brewlabs"];
 
+// Should fetch within a certain radius of the given one
+// Means you need to calculate the cafes that will appear in that radius
 export const fetchCafesByLocation = async (
   latitude: number,
   longitude: number,
@@ -7,7 +9,27 @@ export const fetchCafesByLocation = async (
   return ["Nero", "Plato"];
 };
 
+// Allow for an optional limit to be given
+// Needs to ensure you understand pagination
 export const fetchAllCafes = async (limit?: number) => {
   console.log(`Limit: ${limit}`);
   return limit ? cafes.slice(0, limit) : cafes;
+};
+
+export const fetchCafesWithBeansByBrand = async (beanBrand: string) => {
+  console.log("Cafe with beans by brand");
+  return [
+    "Cafe with beans by brand #1",
+    "Cafe with beans by name #2",
+    "Cafe with beans by name #3",
+  ];
+};
+
+export const fetchCafesWithBeansByName = async (beanName: string) => {
+  console.log("Cafe with beans by name");
+  return [
+    "Cafe with beans by name #1",
+    "Cafe with beans by name #2",
+    "Cafe with beans by name #3",
+  ];
 };
