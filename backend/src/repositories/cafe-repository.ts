914e9ef927000a90e0a8e43404f4a5ca683d05ee
Sean Boonzaier <boonzaiersa@gmail.com>
2026-06-13 14:55:20 +0100

    Implement cafe repository query for fetching cafe by name substring

--- backend/src/repositories/cafe-repository.ts
+++ backend/src/repositories/cafe-repository.ts
@@ -1,8 +1,9 @@
+import type { QueryResult } from "pg";
 import { query } from "../config/db.js";
 import { type Cafe } from "../types/cafe.js";
 
 export class CafeRepository {
-  async fetchAllCafes(limit?: number): Promise<Cafe[] | null> {
+  async fetchAll(limit?: number): Promise<Cafe[] | null> {
     const sql = `
       SELECT
         cafe_name as name,
@@ -15,5 +16,24 @@ export class CafeRepository {
     const res = limit ? await query(sql, [limit]) : await query(sql);
 
     return res.rows as Cafe[];
+  }
+
+  async fetchBySubstr(substr: string): Promise<Cafe[] | null> {
+    console.log(substr);
+    const sql = `
+      SELECT
+        cafe_name as name,
+        cafe_description as description,
+        ST_X(cafe_location) as longitude,
+        ST_Y(cafe_location) as latitude
+      FROM cafes where cafe_name like concat('%', $1::text, '%')
+      `;
+    const res = substr
+      ? await query(sql, [substr]).then(
+          (response: QueryResult) => response.rows,
+        )
+      : null;
+
+    return res;
   }
 }
