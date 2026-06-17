1c0d76f51f47c48162e7b73bcfd148a91b2a7633
Sean Boonzaier <boonzaiersa@gmail.com>
2026-06-17 16:28:30 +0100

    Add basic custom error middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,8 @@
-import express from "express";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 import { cafeRouter } from "./routes/cafe.routes.js";
 const app = express();
 const port = 3000;
@@ -6,6 +10,10 @@ const port = 3000;
 app.use(express.json());
 
 app.use("/v1/cafes", cafeRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send({ status: "fail", message: err.message });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
