fc11a2eb3290394dde3badbd2469eb0c91133e51
Sean Boonzaier <boonzaiersa@gmail.com>
2026-06-25 11:06:47 +0100

    Correct naming of cafes table in prisma

--- backend/prisma/migrations/20260625100544_add_cafes_table/migration.sql
+++ backend/prisma/migrations/20260625100544_add_cafes_table/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
-CREATE TABLE "Cafe" (
+CREATE TABLE "cafes" (
     "id" SERIAL NOT NULL,
     "cafe_name" VARCHAR(50) NOT NULL,
     "cafe_description" VARCHAR(255) NOT NULL,
     "cafe_location" geography(Point, 4326) NOT NULL,
 
-    CONSTRAINT "Cafe_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "cafes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Cafe_cafe_name_key" ON "Cafe"("cafe_name");
+CREATE UNIQUE INDEX "cafes_cafe_name_key" ON "cafes"("cafe_name");
