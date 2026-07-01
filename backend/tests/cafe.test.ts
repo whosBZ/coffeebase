a0e35b07aa49804f0c115c9f2b8cc7588886d0ea
Sean Boonzaier <boonzaiersa@gmail.com>
2026-07-01 17:57:40 +0100

    Update cafe tests to follow best practice

--- backend/tests/cafe.test.ts
+++ backend/tests/cafe.test.ts
@@ -1,13 +1,30 @@
 import request from "supertest";
 import app from "../src/app.js";
-import { pool } from "../src/config/db";
+import { pool, query } from "../src/config/db.js";
+
+
+const seedCafe = async () => {
+  const payload = {
+    name: "Brewlabs",
+    description:
+      "A place to experience the best coffee has to offer in ireland",
+    latitude: "53.33824631274385",
+    longitude: "-6.265785753638034",
+  };
+
+  return await request(app).post("/v1/cafes").send(payload);
+}
 
 afterAll(async () => {
   await pool.end();
 });
 
+beforeEach(async () => {
+  await query("TRUNCATE TABLE cafes RESTART IDENTITY CASCADE");
+});
+
 describe("POST /api/cafes", () => {
-  it("Should validate input via Zod and create a new cafe", async () => {
+  it("Validates input via Zod and create a new cafe", async () => {
     const payload = {
       name: "Brewlabs",
       description:
@@ -25,7 +42,7 @@ describe("POST /api/cafes", () => {
     expect(response.body.message).toBe("Cafe added to database");
   });
 
-  it("Should fail validation and return 400 if properties are missing", async () => {
+  it("Fails validation and return 400 if properties are missing", async () => {
     const badPayload = { name: "" };
 
     const response = await request(app)
@@ -38,3 +55,66 @@ describe("POST /api/cafes", () => {
     expect(response.body.status).toBe("fail");
   });
 });
+
+describe("PATCH /api/cafes", () => {
+  it("Updates previously seeded cafe", async () => {
+
+    await seedCafe()
+
+    const body = {
+      name: "Brewlabs",
+      description: "An amazing place to experience the best coffee has to offer in ireland",
+      latitude: "53.33824631274385",
+      longitude: "-6.265785753638034"
+    }
+    const response = await request(app).patch('/v1/cafes/1').send(body)
+
+    expect(response.status).toBe(200);
+
+    expect(response.body).toHaveProperty("status");
+    expect(response.body.status).toBe("success");
+    expect(response.body.message).toBe("Succesfully updated cafe");
+  })
+})
+
+describe("GET /api/cafes", () => {
+
+  it("Returns a list of cafes", async () => {
+    await seedCafe()
+
+    const response = await request(app).get("/v1/cafes")
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("data");
+    expect(response.body.status).toBe("success");
+    expect(response.body.data).toStrictEqual([{
+      id: 1,
+      name: "brewlabs",
+      description:
+        "A place to experience the best coffee has to offer in ireland",
+      latitude: 53.33824631274385,
+      longitude: -6.265785753638034,
+    }])
+  })
+}
+)
+
+
+describe("DELETE /api/cafes", () => {
+
+  it("Removes previously seeded cafe", async () => {
+    await seedCafe()
+
+    let response = await request(app).delete("/v1/cafes/1");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("status");
+    expect(response.body.status).toBe("success");
+    expect(response.body.message).toBe("Cafe deleted from database")
+
+    response = await request(app).get("/v1/cafes")
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("data");
+    expect(response.body.status).toBe("success");
+    expect(response.body.data).toStrictEqual([])
+  })
+})
+
